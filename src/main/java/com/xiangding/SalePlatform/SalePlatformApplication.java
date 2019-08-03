391b9d553e1f1ea76e39682396454bb93295589c
wuyanqiang <1340151034@qq.com>
2019-08-03 15:31:39 +0800

    创建项目
    创建项目

---
+++ src/main/java/com/xiangding/SalePlatform/SalePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.xiangding.SalePlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalePlatformApplication.class, args);
+	}
+
+}
